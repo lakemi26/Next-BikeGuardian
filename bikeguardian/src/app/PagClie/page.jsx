8a2587c7e9e88376dac73c65be907bfda786e264
IsabelliHelo <isabelliheloizafl@gmail.com>
2023-11-10 23:55:19 -0300

    Update page.jsx

--- bikeguardian/src/app/PagClie/page.jsx
+++ bikeguardian/src/app/PagClie/page.jsx
@@ -6,7 +6,42 @@ export default function PagClie() {
   return (
     <>
       <Navbar/>
-      
+      <Banner
+        imageSrc='/img/Clima-banner.png'
+        altText='Banner clima'
+      />
+      <Banner
+        imageSrc='/img/linhaBoleto.png'
+        altText='Linha boleto'
+      />
+      <Banner
+        imageSrc='/img/Boleto-banner.png'
+        altText='Banner boleto'
+      />
+      <Banner
+        imageSrc='/img/LinhaDet.png'
+        altText='Banner clima'
+      />
+      <Banner
+        imageSrc='/img/Detalhes-banner.png'
+        altText='Banner clima'
+      />
+      <Banner
+        imageSrc='/img/LinhaHist.png'
+        altText='Banner clima'
+      />
+      <Banner
+        imageSrc='/img/Historico-banner.png'
+        altText='Banner clima'
+      />
+      <Banner
+        imageSrc='/img/linhaNot.png'
+        altText='Banner clima'
+      />
+      <Banner
+        imageSrc='/img/Notificacoes-banner.png'
+        altText='Banner clima'
+      />
       <Footer/>
     </>
   )
